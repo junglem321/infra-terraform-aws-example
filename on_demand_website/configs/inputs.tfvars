0d6a94a0f1e4992ad85fbb7a75b1d0f816247570
junglem321 <danielcaceressuavita@gmail.com>
2021-10-02 18:42:14 -0400

    fix shell script and match architecture

--- on_demand_website/configs/inputs.tfvars
+++ on_demand_website/configs/inputs.tfvars
@@ -5,3 +5,11 @@ username = "Admin"
 # password = "Dfsdfsd235K"
 ssh_ip_host = "0.0.0.0/0"
 path_to_public_key = "mykey.pub"
+
+min_size_fe = 1
+max_size_fe = 2
+desired_capacity_fe = 2
+
+min_size_be = 1
+max_size_be = 2
+desired_capacity_be = 2
--- on_demand_website/scripts/ubuntu-fe-init.sh
+++ on_demand_website/scripts/ubuntu-fe-init.sh
@@ -10,4 +10,4 @@ sed -i "s/*:80/*:${PORT}/g" /etc/apache2/sites-enabled/000-default.conf
 
 service apache2 reload
 
-echo "<h1>Hello World from $(hostname) in frontend</h1><a href=\"${target_dns}\">Target backend</a>" > /var/www/html/index.html
+echo "<h1>Hello World from $(hostname) in frontend</h1><a href=\"http://${target_dns}\">Target backend</a>" > /var/www/html/index.html
